a253ca062ad08f29269481d851e11c8c14126412
dev <dev@dev.com>
2023-07-09 18:58:28 +0800

    gateway support APISIX and spring cloud gateway

---
+++ eap-msp/gateway/eap-gateway/src/main/java/org/openea/eap/gateway/GatewayServerApplication.java
@@ -0,0 +1,14 @@
+package org.openea.eap.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        // 启动 Spring Boot 应用
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
---
+++ eap-msp/gateway/eap-gateway/src/main/java/org/openea/eap/gateway/route/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 占位符
+ */
+package org.openea.eap.gateway.route;
