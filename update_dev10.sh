108c0c12999ef0297a5caf7bbcab48d1c4507e4b
dev <dev@dev.com>
2023-07-16 12:05:11 +0800

    update dev10 部署脚本

--- update_dev10.sh
+++ update_dev10.sh
@@ -1,6 +1,10 @@
+# git
+git checkout .
+git pull
 
-
+# maven
 mvn clean install -DskipTests=true -Pdev -PeoaServer
 
-## you should change password in docker-compose-dev10.yml
+# docker
+## you should change password in docker.env.dev10
 sudo docker-compose -f docker-compose-dev10.yml up -d --build
