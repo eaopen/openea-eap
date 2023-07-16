8a29f65e2c8bae3642cd15b983baf49ade623eea
dev <dev@dev.com>
2023-07-16 12:12:28 +0800

    update dev10 部署脚本

--- update_dev10.sh
+++ update_dev10.sh
@@ -7,4 +7,5 @@ mvn clean install -DskipTests=true -Pdev -PeoaServer
 
 # docker
 ## you should change password in docker.env.dev10
+# JDBC_PASSWORD=eap
 sudo docker-compose -f docker-compose-dev10.yml up -d --build
