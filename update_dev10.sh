252135b3cec5f0ebf050ddcf31788ce77450a208
dev <dev@dev.com>
2023-07-16 11:00:27 +0800

    update dev10 部署脚本

--- update_dev10.sh
+++ update_dev10.sh
@@ -3,4 +3,4 @@
 mvn clean install -DskipTests=true -Pdev -PeoaServer
 
 ## you should change password in docker-compose-dev10.yml
-sudo docker-compose -f docker-compose-dev10.yml up -d
+sudo docker-compose -f docker-compose-dev10.yml up -d --build
