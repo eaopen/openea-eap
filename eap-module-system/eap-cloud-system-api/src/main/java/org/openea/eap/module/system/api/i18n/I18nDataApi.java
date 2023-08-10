bbd929b9ff88642704fad820d216d297568829f9
louise <louise@air>
2023-08-10 15:44:08 +0800

    pom 优化, 拆分eap-dependencies和eap-cloud-dependencies

--- eap-module-system/eap-cloud-system-api/src/main/java/org/openea/eap/module/system/api/i18n/I18nDataApi.java
+++ eap-module-system/eap-cloud-system-api/src/main/java/org/openea/eap/module/system/api/i18n/I18nDataApi.java
@@ -4,9 +4,14 @@ package org.openea.eap.module.system.api.i18n;
  * I18n/语言/翻译 API
  * 
  */
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.openea.eap.module.system.enums.ApiConstants;
+import org.springframework.cloud.openfeign.FeignClient;
+
 @FeignClient(name = ApiConstants.NAME) // TODO 芋艿：fallbackFactory =
 @Tag(name =  "RPC 服务 - i18n/语言/翻译")
-public interface LanguageDtaApi {
+public interface LanguageDataApi {
 
     String PREFIX = ApiConstants.PREFIX + "/i18n-data";
 }
