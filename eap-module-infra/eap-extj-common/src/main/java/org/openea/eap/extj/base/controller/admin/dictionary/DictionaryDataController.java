9c6a835e65a2071ca8c8d454201e2def767f531c
golmin <43638057+golmin@users.noreply.github.com>
2023-08-31 10:43:11 +0800

    fix dictionaryData

--- eap-module-infra/eap-extj-common/src/main/java/org/openea/eap/extj/base/controller/admin/dictionary/DictionaryDataController.java
+++ eap-module-infra/eap-extj-common/src/main/java/org/openea/eap/extj/base/controller/admin/dictionary/DictionaryDataController.java
@@ -1,32 +1,25 @@
 package org.openea.eap.extj.base.controller.admin.dictionary;
 
+import cn.hutool.core.map.MapUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.openea.eap.extj.base.ActionResult;
 import org.openea.eap.extj.base.controller.SuperController;
 import org.openea.eap.extj.base.entity.DictionaryDataEntity;
 import org.openea.eap.extj.base.entity.DictionaryTypeEntity;
-import org.openea.eap.extj.base.model.DictionaryDataExportModel;
 import org.openea.eap.extj.base.service.DictionaryDataService;
 import org.openea.eap.extj.base.service.DictionaryTypeService;
 import org.openea.eap.extj.base.vo.ListVO;
-import org.openea.eap.extj.util.JsonUtil;
-import org.openea.eap.extj.util.treeutil.SumTree;
-import org.openea.eap.extj.util.treeutil.newtreeutil.TreeDotUtils;
-import org.openea.eap.framework.common.pojo.CommonResult;
-import org.openea.eap.module.system.controller.admin.dict.vo.data.DictDataSimpleRespVO;
-import org.openea.eap.module.system.convert.dict.DictDataConvert;
+import org.openea.eap.extj.util.StringUtil;
 import org.openea.eap.module.system.dal.dataobject.dict.DictDataDO;
 import org.openea.eap.module.system.service.dict.DictDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.*;
+import java.util.stream.Collectors;
 
-import static org.openea.eap.framework.common.pojo.CommonResult.success;
 
 @RestController
 @Tag(name = "数据字典", description = "dictionary")
@@ -41,7 +34,7 @@ public class DictionaryDataController  extends SuperController<DictionaryDataSer
 
     @ApiOperation("获取数据字典列表(分类+内容)")
     @GetMapping("/All-simple")
-    public ActionResult<ListVO<Map<String, Object>>> allBindDictionarySimple() {
+    public ActionResult<ListVO<Map<String, Object>>> allBindDictionarySimple(@RequestBody(required = false) String keys) {
         List<DictionaryTypeEntity> dictionaryTypeList = dictionaryTypeService.getList();
         List<Map<String, Object>> list = new ArrayList<>();
         for (DictionaryTypeEntity dictionaryTypeEntity : dictionaryTypeList) {
@@ -63,6 +56,12 @@ public class DictionaryDataController  extends SuperController<DictionaryDataSer
             htItem.put("value", dictDataDO.getValue());
             htItem.put("parentId", dictDataDO.getParentId());
             list.add(htItem);
+        }
+
+        if (keys!=null && StringUtil.isNotEmpty(keys)){
+            String finalKeys = keys;
+            list = list.stream().filter(t -> finalKeys.contains(MapUtil.getStr(t, "dictType"))).collect(Collectors.toList());
+
         }
         ListVO<Map<String, Object>> vo = new ListVO<>();
         vo.setList(list);
